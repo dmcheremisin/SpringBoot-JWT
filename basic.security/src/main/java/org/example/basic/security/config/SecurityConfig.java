885314e480506ec1a8899572b46394f0a3a67503
Dmitrii Cheremisin <Dmitrii_Cheremisin@epam.com>
2020-03-16 13:31:24 +0300

    configure basic authentication

---
+++ basic.security/src/main/java/org/example/basic/security/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package org.example.basic.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+            .withUser("dima")
+            .password("password")
+            .roles("ADMIN")
+            .and()
+            .withUser("anya")
+            .password("yoga")
+            .roles("MANAGER");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}

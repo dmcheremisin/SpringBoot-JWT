74584f55a5808cfde1d18bcd51c09c2cc78d5c6f
Dmitrii Cheremisin <Dmitrii_Cheremisin@epam.com>
2020-03-16 13:09:50 +0300

    Basic Security App

---
+++ basic.security/src/main/java/org/example/basic/security/BasicSecurityApp.java
@@ -0,0 +1,13 @@
+package org.example.basic.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSecurityApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSecurityApp.class, args);
+	}
+
+}
